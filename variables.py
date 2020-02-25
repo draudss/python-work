d3e52cca4f603bb35221b61ffc82e882c282bf27
nsahsspecial1 <nsahsspecial1@usi.uncommonschools.org>
2020-02-25 15:17:12 -0500

    assignment one

---
+++ variables.py
@@ -0,0 +1,23 @@
+name = "lucas"
+age = 16
+
+print("My name is", 'lucas' ,"and I am", age)
+
+a=1
+futureage=a+age
+
+print("Next year i will be", futureage )
+
+randomint=5
+randomNone= None
+randomString= "sup"
+randomFloat= 2.2
+randomBool= None
+
+print (type(randomint))
+print (type(randomNone))
+print (type(randomString))
+print (type(randomFloat))
+print (type(randomBool))
+
+print("My name is" + 'lucas' + "and I am" + str(age))
